7c86e3bb47083a613cfccb562bfe1513669d88e5
Dev Nandan <a.d.devnandan@gmail.com>
2023-08-06 15:40:33 +0530

    gallery-finished, share started

--- Frontend/interface/gallery.js
+++ Frontend/interface/gallery.js
@@ -1,25 +1,18 @@
-
-var file = [];
-document.getElementById('file').addEventListener('change', function () {
-    if (this.files.length > 0) {
-        var fileSizeInMB = this.files[0].size / (1024 * 1024);
-      file.push({
-        no : file.length + 1,
-        type: this.files[0].type,
-        name: this.files[0].name,
-        size: fileSizeInMB.toFixed(2),
-        photo : this.files[0] 
-      });
-      buildTable(file);
+document.getElementById("file-input").addEventListener("change", function(event) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+  
+      reader.onload = function() {
+        const imageDataURL = reader.result;
+        const imagePreview = document.getElementById("gallery");
+        var image = `
+        <img src="${imageDataURL}">
+        `
+        imagePreview.innerHTML = image ;
+      };
+  
+      reader.readAsDataURL(file);
     }
   });
- buildTable(file);
-function buildTable(data){
-    var gallery = document.getElementById('gallery');
-    for(var i = 0; i < data.length; i++){
-        var row = `
-        <img src="${data[i].photo}">
-        `
-        gallery.innerHTML += row;
-    }
-}
+  
