5a51ae564021b2dec2e6d14dceba29e3adbe5646
Dev Nandan <a.d.devnandan@gmail.com>
2023-08-08 10:54:33 +0530

    90%

--- Frontend/interface/gallery.js
+++ Frontend/interface/gallery.js
@@ -1,18 +1,33 @@
-document.getElementById("file-input").addEventListener("change", function(event) {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-  
-      reader.onload = function() {
-        const imageDataURL = reader.result;
-        const imagePreview = document.getElementById("gallery");
-        var image = `
-        <img src="${imageDataURL}">
-        `
-        imagePreview.innerHTML = image ;
+const fileInput = document.getElementById('file');
+const gallery = document.getElementById('gallery');
+var photos = [];
+
+document.getElementById('file').addEventListener('change', function () {
+  if (this.files.length > 0) {
+    var fileSizeInMB = this.files[0].size / (1024 * 1024);
+    photos.push(this.files[0]);
+    buildGallery(photos);
+  }
+});
+
+function buildGallery(data) {
+  let galleryHTML = ''; 
+  for (var i = 0; i < data.length; i++) {
+    let file = data[i];
+    let fileType = file.type;
+    console.log(fileType);
+    let validType = ['image/png', 'image/jpg', 'image/jpeg', 'image/gif'];
+    if (validType.includes(fileType)) {
+      let fileReader = new FileReader();
+      fileReader.onload = () => {
+        let fileURL = fileReader.result;
+        let imgTag = `<img src="${fileURL}">`;
+        galleryHTML += imgTag; 
+        gallery.innerHTML = galleryHTML; 
       };
-  
-      reader.readAsDataURL(file);
+      fileReader.readAsDataURL(file);
+    } else {
+      alert("Invalid file type");
     }
-  });
-  
+  }
+}
