a7cc21b13c4754f423ed65b653b7f23606a94145
Viserion-7 <harigovindbiju@gmail.com>
2023-08-09 01:38:15 +0530

    Final Changes

--- Frontend/src/drive.js
+++ Frontend/src/drive.js
@@ -1,55 +1,57 @@
-var file = [  {
-  type: "JPEG",
-  name: "image1.jpg",
-  size: "1.5mb"
-},
-{
-  type: "PNG",
-  name: "image2.png",
-  size: "2.2mb"
-},
-{
-  type: "PDF",
-  name: "document1.pdf",
-  size: "5mb"
-},
-{
-  type: "DOCX",
-  name: "document2.docx",
-  size: "0.8mb"
-},
-{
-  type: "MP3",
-  name: "song1.mp3",
-  size: "3.7mb"
-},
-{
-  type: "TXT",
-  name: "textfile.txt",
-  size: "0.2mb"
-},
-{
-  type: "CSV",
-  name: "data.csv",
-  size: "1.1mb"
-},
-{
-  type: "GIF",
-  name: "animation.gif",
-  size: "0.6mb"
-},
-{
-  type: "MP4",
-  name: "video.mp4",
-  size: "10mb"
-},
-{
-  type: "ZIP",
-  name: "archive.zip",
-  size: "7.5mb"
-}];
+var file = [
+  {
+    type: "JPEG",
+    name: "image1.jpg",
+    size: "1.5mb",
+  },
+  {
+    type: "PNG",
+    name: "image2.png",
+    size: "2.2mb",
+  },
+  {
+    type: "PDF",
+    name: "document1.pdf",
+    size: "5mb",
+  },
+  {
+    type: "DOCX",
+    name: "document2.docx",
+    size: "0.8mb",
+  },
+  {
+    type: "MP3",
+    name: "song1.mp3",
+    size: "3.7mb",
+  },
+  {
+    type: "TXT",
+    name: "textfile.txt",
+    size: "0.2mb",
+  },
+  {
+    type: "CSV",
+    name: "data.csv",
+    size: "1.1mb",
+  },
+  {
+    type: "GIF",
+    name: "animation.gif",
+    size: "0.6mb",
+  },
+  {
+    type: "MP4",
+    name: "video.mp4",
+    size: "10mb",
+  },
+  {
+    type: "ZIP",
+    name: "archive.zip",
+    size: "7.5mb",
+  },
+];
 
-document.getElementById('file').addEventListener('change', function () {
+document.getElementById("file").addEventListener("change", function () {
   if (this.files.length > 0) {
     var fileSizeInMB = this.files[0].size / (1024 * 1024);
     file.push({
@@ -61,7 +63,7 @@ document.getElementById('file').addEventListener('change', function () {
   }
 });
 
-$('#search-box').on('keyup', function () {
+$("#search-box").on("keyup", function () {
   var value = $(this).val();
   var data = SearchTable(value, file);
   buildTable(data);
@@ -81,9 +83,9 @@ function SearchTable(value, data) {
 }
 
 function buildTable(data) {
-  var table = document.getElementById('myTable');
+  var table = document.getElementById("myTable");
   // Clear the existing table rows before rebuilding
-  table.innerHTML = '';
+  table.innerHTML = "";
 
   for (var i = 0; i < data.length; i++) {
     var row = `<tr>
--- Frontend/src/gallery.js
+++ Frontend/src/gallery.js
@@ -1,8 +1,8 @@
-const fileInput = document.getElementById('file');
-const gallery = document.getElementById('gallery');
+const fileInput = document.getElementById("file");
+const gallery = document.getElementById("gallery");
 var photos = [];
 
-document.getElementById('file').addEventListener('change', function () {
+document.getElementById("file").addEventListener("change", function () {
   if (this.files.length > 0) {
     var fileSizeInMB = this.files[0].size / (1024 * 1024);
     photos.push(this.files[0]);
@@ -11,19 +11,19 @@ document.getElementById('file').addEventListener('change', function () {
 });
 
 function buildGallery(data) {
-  let galleryHTML = ''; 
+  let galleryHTML = "";
   for (var i = 0; i < data.length; i++) {
     let file = data[i];
     let fileType = file.type;
     console.log(fileType);
-    let validType = ['image/png', 'image/jpg', 'image/jpeg', 'image/gif'];
+    let validType = ["image/png", "image/jpg", "image/jpeg", "image/gif"];
     if (validType.includes(fileType)) {
       let fileReader = new FileReader();
       fileReader.onload = () => {
         let fileURL = fileReader.result;
         let imgTag = `<img src="${fileURL}">`;
-        galleryHTML += imgTag; 
-        gallery.innerHTML = galleryHTML; 
+        galleryHTML += imgTag;
+        gallery.innerHTML = galleryHTML;
       };
       fileReader.readAsDataURL(file);
     } else {
